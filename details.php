4d32cab7746d5577ce4d61dfa0ab6c585b356bd0
Dirceu102650 <dirceulh@hotmail.com>
2018-06-26 15:31:23 -0300

    Update details.php

--- details.php
+++ details.php
@@ -61,10 +61,10 @@ else
 <head>
     <meta charset="UTF-8">
     <title>Dados</title>
-	<link rel="icon" href="img/home.png" type="image/x-icon" />
-	<link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="icon" href="img/home.png" type="image/x-icon" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
     <link href="css/bootstrap-theme.min.css" rel="stylesheet" />
-	<link href="css/font-awesome.min.css" rel="stylesheet" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
     <script type="text/javascript" src="js/jquery.min.js" ></script>
     <script type="text/javascript" src="js/bootstrap.min.js" ></script>
 </head>
@@ -80,56 +80,54 @@ else
 </nav>
 <div class="container">
    <table class="table table-bordered">
-		<tr >
-				<th>Usuário</th>
-				<th>Acertos</th>
-				<th>Erros</th>
-				<th>Teste iniciado em</th>
-				<th>Teste terminado em</th>
-				<th>Realizado em</th>				
-
-		</tr>
+	<tr >
+		<th>Usuário</th>
+		<th>Acertos</th>
+		<th>Erros</th>
+		<th>Teste iniciado em</th>
+		<th>Teste terminado em</th>
+		<th>Realizado em</th>				
+	</tr>
 <?php foreach($data as $d){ 
  $finalized_at = $d->finalized_at;
  $created_at = $d->created_at;
  $date_time  = new DateTime($created_at);
  $diff       = $date_time->diff( new DateTime($finalized_at));
 ?>
-		<tr>
-			<td><?=$d->user?></td>		
-			<td><?=$d->hits?></td>
-			<td><?=$d->error?></td>
-			<td><?= date("d/m/Y H:i:s", strtotime($d->created_at)); ?></td>
-			<td><?= date("d/m/Y H:i:s", strtotime($d->finalized_at)); ?></td>
-            <td><?=$diff->format('%s segundo(s)');?></td>
-		</tr>
+	<tr>
+	        <td><?=$d->user?></td>		
+		<td><?=$d->hits?></td>
+		<td><?=$d->error?></td>
+		<td><?= date("d/m/Y H:i:s", strtotime($d->created_at)); ?></td>
+		<td><?= date("d/m/Y H:i:s", strtotime($d->finalized_at)); ?></td>
+                <td><?=$diff->format('%s segundo(s)');?></td>
+	</tr>
        <?php } ?>		
     </table>
 </div>
-
 <div class="container">
    <table class="table table-bordered">
-		<tr >
-				<th>Pergunta</th>
-				<th>Resposta Correta</th>
-
+	<tr >
+		<th>Pergunta</th>
+		<th>Resposta Correta</th>
+        </tr>
         <?php foreach($data_result as $data){ ?>
 
-		<tr>
-			<td><?=$data->body?></td>
-			<td><?=$data->description_options?></td>
+	<tr>
+	     <td><?=$data->body?></td>
+	     <td><?=$data->description_options?></td>
 
-		</tr>
+	</tr>
        <?php } ?>		
     </table>
 </div>
 
 <div class="container">
    <table class="table table-bordered">
-		<tr >
-
-				<th>Perguntas que você acertou</th>
-				<th>Sua Resposta</th>
+	<tr>
+		<th>Perguntas que você acertou</th>
+		<th>Sua Resposta</th>
+		
         <?php foreach($data_resulter as $data_r){
             $sql = "SELECT  body,description_options FROM questions where answer_id = '".$data_r->answers_id. "'";
 			$stm = $connection->prepare($sql);
@@ -151,8 +149,8 @@ else
 <div class="container">
    <table class="table table-bordered">
 		<tr >
-				<th>Pergunta que você errou</th>
-				<th>Sua resposta</th>
+			<th>Pergunta que você errou</th>
+			<th>Sua resposta</th>
 
         <?php foreach ($data_resul as $e ){
             $sql = "SELECT  question_id , body FROM question_options where id = '".$e->answers_id."' ";
@@ -165,8 +163,8 @@ else
 			<?php foreach($data_answer_not as $data_a_n)
 			$sql = "SELECT  body FROM questions where id = '".$data_a_n->question_id."' ";
 			$stm = $connection->prepare($sql);
-	        $stm->execute();
-	        $data_answer_question_not = $stm->fetchAll(PDO::FETCH_OBJ);
+	                $stm->execute();
+	                $data_answer_question_not = $stm->fetchAll(PDO::FETCH_OBJ);
 			{?>
 			<?php foreach($data_answer_question_not as $data_a_q_n) { ?>
 			<td><?=$data_a_q_n->body?></td>
@@ -177,6 +175,5 @@ else
        <?php } ?>		
     </table>
 </div>
-
 </body>
 </html>
